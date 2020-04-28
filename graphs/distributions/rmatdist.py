f13fd66bd29b76a74aba85e8b31e858dd0786107
Jason Yik <jyik@usc.edu>
2020-04-28 12:41:22 -0700

    gen graphs

--- graphs/distributions/rmatdist.py
+++ graphs/distributions/rmatdist.py
@@ -6,28 +6,30 @@
 # Generation of randomized edges with different distributions for graph
 #
 
-# TODO: fix n and nnz, make arguments be probabilities a b c
-
 import sys
 import snap
 import time
 
 def main():
-	if len(sys.argv) < 3:
-		print("enter n and nnz as arguments")
+	if len(sys.argv) < 4:
+		print("enter a b c as arguments")
 		return
 
-	n = int(sys.argv[1])
-	e = int(sys.argv[2])
+	n = 32768
+	e = 262144
+
+	a = float(sys.argv[1])
+	b = float(sys.argv[2])
+	c = float(sys.argv[3])
 
 	now = time.time()
 	Rnd = snap.TRnd()
 	Rnd.PutSeed(int(now))
 
 	for i in range(5):
-		Graph = snap.GenRMat(n, e, .25, .25, .25, Rnd) 
+		Graph = snap.GenRMat(n, e, a/100, b/100, c/100, Rnd) 
 
-		filestr = str(n) + "_" + str(e) + "_" + str(i)
+		filestr = str(int(a)) + "_" + str(int(b)) + "_" + str(int(c)) + "_" + str(i)
 
 		f = open(filestr, "w+")
 
